85ba9bf4c1a68920a0932c47262a5c391cb34ed1
Ethan Hu <hu.ethan.ca@gmail.com>
2024-10-19 00:07:15 -0400

    Update

--- Assets/Scripts/PlayNarration.cs
+++ Assets/Scripts/PlayNarration.cs
@@ -3,18 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class PlayNarration : MonoBehaviour
 {
-    public AudioSource introduction;
-    public AudioSource Stanza1;
-    public AudioSource Stanza2;
-    public AudioSource Stanza3;
-    public AudioSource Stanza4;
-    public AudioSource Stanza5;
-    void Start()
-    {
-            
-    }
+    public Transform player;
+    public float playerPositionToPlay = 0;
+    private bool isPlayed = false;
     void Update()
     {
-        
+        if(!isPlayed && player.transform.position.x > playerPositionToPlay)
+        {
+            GetComponent<AudioSource>().Play();
+            isPlayed = true;
+        }
     }
 }
